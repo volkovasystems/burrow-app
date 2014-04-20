c7da58c6d8f334f08b38c1c1e9b3addee76cbb49
Regynald Ventura <regynaldventura@gmail.com>
2014-04-20 11:59:23 +0800

    with socket hang_error

--- test/basic-server-client/client.js
+++ test/basic-server-client/client.js
@@ -1,58 +1,60 @@
 var http = require( "http" );
 var async = require( "async" );
-/*
+
 var stringList = [
-	"apple", "mangoes"
+	"dog",
+	"odin",
+	"magic"
 	];
-*/
-var requestBruteforce = function requestBruteforce( string, callback ){
-	var options = {
-	  "host": "127.0.0.1",
-	  "port": 8080,
-	   "path": "?requestTime = "+Date.now()
-	};
-	http.get( options, 
-		function onResponse( response ){
-			response.on( "data",
-				function onData( data ){
-					data = JSON.parse (data);
-					var rawString = data.rawString;				// string from permutation matched with hash
-					var hashedString = data.hashedString;		// hash of rawString from permutation matched from rawString 
-					var originalString = data.originalString;	// string from server
-					var responseDuration = Date.now( ) - data.responseTime; // response time of server
 
-					data.responseDuration = responseDuration;
+	var requestBruteforce = function requestBruteforce( string, callback ){
+		var options = {
+			"host": "127.0.0.1",
+			"port": 8080,
+			"path":"?rawString=" + string + "&requestTime=" + Date.now()
+		};
 
-					callback(null, data);
+		http.get( options,
+			function onResponse( response ){
+				response.on( "data",
+					function onData( data ){
+						data = JSON.parse ( data );
+						data.responseDuration = parseInt(Date.now() - data.requestTime);
+						data.responseDuration= responseDuration;
+						callback( null , data );
 					} );
-		} )
+			} )
 		.on( "error",
 			function onError( error ) {
-
-					callback(error);
-
-	  			console.log("Got error: " + error.message);
+				console.log("GET error: " + error.message);
 			} );
-};
+	};
 
-/*
-var bruteForceEngineList = [ ];
+	var bruteForceEngineList = [ ];
 
-for(index=0; index<stringList.length;index++){
-	bruteForceEngineList.push( function engine( callback ){
-		requestBruteforce( stringList[ index ],
-			function onCallback( error, result ){
-				callback( error, result );
-			} );
-	} );
-}
+	for(index=0; index<stringList.length;index++){
+		bruteForceEngineList.push( function engine( callback ){
+			var currentString= stringList[ index ];
+			requestBruteforce( currentString,
+				function onCallback( error , result ){
+					callback( error, result );
+				} );
+		} );
+	}
 
+	async.series( bruteForceEngineList,
+		function onFinal( error, results ){
+			if( error ){
+				console.log( "ASYNC ERROR: " + error );
+			}else{
+				console.log( "results:"+ results );
+			}
+		} );
 
-async.series( bruteForceEngineList,
-	function onFinal( error, results ){
+	process.on('uncaughtException', function(err) {
+		console.log('Caught exception: ' + err);
+	});
 
-console.log (results);
-
-	} );
-
-*/
+	setTimeout(function() {
+		console.log('This will still run.');
+	}, 500);
--- test/basic-server-client/server.js
+++ test/basic-server-client/server.js
@@ -1,7 +1,14 @@
-var http = require( "http" );
+var http = require( "http" )
+, domain = require('domain')
+, serverDomain = domain.create();
+
 var util = require( "util" );
 var url = require( "url" );
 var crypto = require( "crypto" );
+var async = require( "async" );
+
+var domain = require ( "domain" );
+var d = domain.create ();
 
 var work = require( "./work.js" ).work;
 
@@ -11,61 +18,69 @@ var md5Hash = function md5Hash( string ){
 	return md5.digest( "hex" ).toString( );
 };
 
-var server = http.createServer( );
+serverDomain.run ( function () {
+	var server = http.createServer( function onCreate ( request,response ){
+		var requestDomain = domain.create();
+		requestDomain.add( request );
+		requestDomain.add( response );
+
+		requestDomain.on("error", function ( error ) {
+			console.log( "Error", error, request.url );
+			requestDomain.dispose( );
+		});
+
+		// Wait 5 seconds before responding
+		setTimeout(function ( ){
+			response.writeHead( 200, { "Content-Type" : "text/plain" } );
+			response.end( "Hello World\n" );
+		}, 5000);
+	}).listen( 8080 );
 
 server.on( "request",
 	function onRequest( request,response ){
-
 		var urlData = url.parse( request.url, true );
-		var string = urlData.query.string;
-		var requestTime =parseInt(urlData.query.requestTime);		
-
-
-		//var requestDuration= Date.now () - parseInt(urlData.query.requestTime);
-		//var hashString = urlData.query.hashString;
-		//var guessLength = urlData.query.guessLength;
+		var rawString = urlData.query.rawString;
+		var requestTime = parseInt( urlData.query.requestTime );		
+		var requestDuration = Date.now () - parseInt( requestTime );
+		var hashedString = md5Hash( rawString );
 		
-		if (string&&requestTime){
-				response.writeHead( 200, { "Content-Type": "text/plain" } );
-				response.end( JSON.stringify( {
-				"rawString":string,
-				"requestTime":requestTime,
-				//"requestDuration":requestDuration
-				},null, "\t"));	
-
-}
-
-
-//console.log (__dirname);
-//var hash = string ; //md5Hash( string );
-/*
-		var command = "cd ../basic-server-client/ && javac BruteForce.java -d . && java BruteForce " + string+ " " + string.Length;
-		work( command, 
-			function callback( error, isValid, output ){
-				console.log (error);
-			var newString=JSON.parse(output);
-				response.writeHead( 200, { "Content-Type": "text/plain" } );
-				response.end( JSON.stringify( {
-					//"requestTime": requestTime,
-					//"hashedString (hash of rawString)": md5Hash(newString.result),
-					//"originalString (String that matched from given Hash)": newString.result, 
-					//"Processing Duration (client processingTime)": newString.duration
-				}, null, "\t" ) );
-
-	
-//console.log (output);
-			} );
-*/
+		var command = "cd ../basic-server-client/ && javac BruteForce.java -d . && java BruteForce " + hashedString + " " +parseInt(rawString.length);
 		
-			
-
-			} );
+			work( command,
+				function callback( error, isValid, output ){
+					if ( error ){
+						response.writeHead( 200, { "Content-Type": "text/plain" } );
+						response.end ( error );
+					}else{
+						var data = JSON.parse( output );
+						response.writeHead( 200, { "Content-Type": "text/plain" } );
+						response.end(JSON.stringify ( {
+							"requestDuration": requestDuration,
+		                    "responseTime": Date.now( ),
+		                    "rawString": rawString,
+		                    "hashedString": hashedString,
+		                    "originalString": data.result, 
+		                    "processDuration": data.duration
+		                }, null, "\t" ) );
+					}
+				} );
+		} );
 
 server.on( "listening",
 	function onListening( ){
 		console.log( "The server is listening." );
 	} );
 
-server.listen( 8080 );
+process.on("uncaughtException", function(error) {
+	console.error ("Caught exception: "+ error);
+} );
 
+setTimeout(function() {
+	console.log("This will still run.");
+}, 500);
 
+d.on('error', function(err) {
+  console.error(err);
+});
+
+});
