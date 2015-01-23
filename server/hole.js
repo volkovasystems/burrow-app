4c89eebda786ac6ec4667fdab69abfe80a4d9494
Richeve S. Bebedor <volkovasystems@gmail.com>
2015-01-23 21:34:30 +0800

    Minor updates.

--- server/hole.js
+++ server/hole.js
@@ -1,3 +1,8 @@
+var async = require( "async" );
+var fs = require( "fs" );
+var path = require( "path" );
+var _ = require( "lodash" );
+
 var Hole = function Hole( hole, holeSet ){
 	this.initialize( hole, holeSet );
 
@@ -14,11 +19,62 @@ Hole.prototype.configure = function configure( hole, holeSet ){
 	var self = this;
 	hole.on( "connection",
 		function onConnection( socket ){
-			socket.on( "ping",
-				function onPing( ){
-					socket.emit( "ping", Date.now( ) );
+			self.setSocket( socket );
+
+			self.listenToCommand( );
+
+			socket.on( "disconnect",
+				function onDisconnect( ){
+					self.removeSocket( );
 				} );
 		} );
+};
+
+Hole.prototype.setSocket = function setSocket( socket ){
+	this.socket = socket;
+};
+
+Hole.prototype.getSocket = function getSocket( ){
+	return this.getSocket( );
+};
+
+Hole.prototype.removeSocket = function removeSocket( ){
+	this.socket = null;
+};
+
+Hole.prototype.listenToCommand = function listenToCommand( ){
+	this.getSocket( )
+		.on( "command",
+			function onCommand( command, callback ){
+				async.waterfall( [
+					function findCommand( callback ){
+
+					},
+
+					function extractParameterSet( callback ){
+
+					},
+
+					function executeCommand( callback ){
+
+					},
+
+					function compileResult( callback ){
+
+					}
+				],
+					function lastly( ){
+
+					} );
+			} );
+};
+
+Hole.prototype.findCommand = function findCommand( ){
+	var 
+};
+
+Hole.prototype.executeCommand = function executeCommand( ){
+
 };
 
 exports.Hole = Hole;
