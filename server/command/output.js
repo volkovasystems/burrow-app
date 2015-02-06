316bf505b16d8e0c0c51c666da172a5b48f6b30d
Richeve S. Bebedor <volkovasystems@gmail.com>
2015-02-06 22:49:44 +0800

    Fixed issue in output command.

--- server/command/output.js
+++ server/command/output.js
@@ -1,4 +1,19 @@
-var output = function output( callback ){
+var _ = require( "lodash" );
+
+var output = function output( ){
+	var callback = _.last( _.toArray( arguments ) );
+
+	if( arguments.length > 1 ){
+		var outputPhrase = _.initial( _.toArray( arguments ) ).join( " " );
+
+		if( _.isEmpty( this.outputPhrase ) ){
+			this.outputPhrase = outputPhrase;
+		
+		}else{
+			this.outputPhrase = [ this.outputPhrase, outputPhrase ].join( " " ).replace( /\s+/, " " );
+		}
+	}
+
 	callback( null, {
 		"type": "text",
 		"text": this.outputPhrase || this.commandPhrase
