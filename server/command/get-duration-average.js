b4efaa400f3d3e089f193a5bd20219f759c45e69
Richeve S. Bebedor <volkovasystems@gmail.com>
2015-02-15 14:59:58 +0800

    Modified format. Use duration instead of time.

--- server/command/get-duration-average.js
+++ server/command/get-duration-average.js
@@ -5,41 +5,40 @@ var getDurationAverage = function getDurationAverage( referenceList, callback ){
 	var Grub =	mongoose.model( "Grub" );
 
 	Grub.aggregate( [
-						{ $match :
-							{ reference : { $in: referenceList } }
-						},
+		{ 
+			"$match": { "reference": { "$in": referenceList } }
+		},
+		{ 
+			"$group": {
+				"_id": {
+					"command": "$command",
+					"reference": "$reference"
+				},
+				"totalRequestDuration": { "$sum": "$duration.requestDuration" },
+				"totalResponseDuration": { "$sum": "$duration.responseDuration" },
+				"overAllDuration": { "$sum": "$duration.totalDuration" }
+			}
+		},
+		
+		{ 
+			"$group": {
+				"_id" : {
+					"command" : "$_id.command",
+					"reference" : "$_id.reference"
+				},
+				"averageRequestDuration": { "$avg": "$totalRequestDuration" },
+				"averageResponseDuration": { "$avg": "$totalResponseDuration" },
+				"averageTotalDuration": { "$avg": "$overAllDuration" }
+			}
+		}
+	],
 
-						{ $group:
-							{
-								_id : {
-									command: "$command",
-									reference: "$reference"
-								},
-								totalRequestTime : { $sum: "$duration.requestTime" },
-								totalResponseTime : { $sum: "$duration.responseTime" },
-								totalDurationTime : { $sum: "$duration.totalDuration" }
-							}
-						},
-						
-						{ $group:
-							{
-								_id : {
-									command : "$_id.command",
-									reference : "$_id.reference"
-								},
-								averageRequestTime : { $avg: "$totalRequestTime" },
-								averageResponseTime : { $avg: "$totalResponseTime" },
-								averageDurationTime : { $avg: "$totalDurationTime" }
-							}
-						}
-					],
-
-					function ( error, result ){
-						if( error ){
-							callback( false );
-						}else{
-							callback( result );
-						}
+	function ( error, result ){
+		if( error ){
+			callback( false );
+		}else{
+			callback( result );
+		}
 	} );	
 };
 exports.getDurationAverage = getDurationAverage;
