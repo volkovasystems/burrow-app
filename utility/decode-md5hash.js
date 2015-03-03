9425f900f3df448013e8482b8eeb7e05d8dbce07
regynaldventura <regynaldventura@gmail.com>
2015-03-03 09:33:56 +0800

    removed buffer limit.

--- utility/decode-md5hash.js
+++ utility/decode-md5hash.js
@@ -6,7 +6,6 @@ var decodeMD5Hash = function decodeMD5Hash( hash, dictionary, limitLength, start
 	var task = childprocess.spawn( "java", [		
 		"-server",
 		"-XX:-UseConcMarkSweepGC",
-		"-Xmx1g",
 		"-XX:MaxGCPauseMillis=500",		
 		"revertHashByPartition.revertHashByPartition",
 		hash,
