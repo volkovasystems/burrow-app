327d84ed3b39076a7fb7d283de17c9a709acb508
regynaldventura <regynaldventura@gmail.com>
2015-03-09 04:08:01 +0800

    remove decimal in milliseconds

--- client/script/request-response-duration.js
+++ client/script/request-response-duration.js
@@ -1,15 +1,15 @@
 ( function module( ){
 	var requestResponseDuration = function requestResponseDuration( durationData ){
-		var requestTime = moment( durationData.requestTime, "DD/MM/YYYY HH:mm:ss", true  );
-		var responseTime = moment( durationData.responseTime, "DD/MM/YYYY HH:mm:ss", true );
+		var requestTime = moment( durationData.requestTime, "DD/MM/YYYY HH:mm:ss", false  );
+		var responseTime = moment( durationData.responseTime, "DD/MM/YYYY HH:mm:ss", false );
 
 		var finalTime = Date.now( );
 
-		finalTime = moment( finalTime, "DD/MM/YYYY HH:mm:ss", true );
+		finalTime = moment( finalTime, "DD/MM/YYYY HH:mm:ss", false );
 
-		var requestDuration = responseTime.diff( requestTime, "DD/MM/YYYY HH:mm:ss", true );
-		var responseDuration = finalTime.diff( responseTime, "DD/MM/YYYY HH:mm:ss", true );
-		var totalDuration = finalTime.diff( requestTime, "DD/MM/YYYY HH:mm:ss", true );
+		var requestDuration = responseTime.diff( requestTime, "DD/MM/YYYY HH:mm:ss", false );
+		var responseDuration = finalTime.diff( responseTime, "DD/MM/YYYY HH:mm:ss", false );
+		var totalDuration = finalTime.diff( requestTime, "DD/MM/YYYY HH:mm:ss", false );
 
 		return {
 			"requestDuration": requestDuration,
